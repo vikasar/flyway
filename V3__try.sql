76eecc75dfad193b68f2d90071c468dfce7e68eb
Vikas <vikas.rathod@gmail.com>
2021-07-04 16:40:08 +0530

    8

--- V3__try.sql
+++ V3__try.sql
@@ -3,3 +3,5 @@ insert into person values (3,'TRY2');
 insert into person values (4,'newBranch');
 insert into person values (5,'newBranchAfterCommit');
 insert into person values (6,'newBranchAfterCommit6');
+insert into person values (7,'newBranchAfterCommit7');
+insert into person values (8,'newBranchAfterCommit8');
